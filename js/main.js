e2e73191ebdf763b8816dbca850081c0981d5f1c
Luiggi Yantas Inocente <luiggiyantas@gmail.com>
2023-01-01 16:48:04 -0500

    filtrar portafolio

--- js/main.js
+++ js/main.js
@@ -24,3 +24,54 @@
         }
     })
 })();
+
+//  ==============================================================
+//  porfolio filter - popup 
+//  ============================================================== 
+
+(()=>{
+    const filterContainer = document.querySelector('.portfolio-filter'),
+        portfolioItemsContainer = document.querySelector('.portfolio-items'),
+        portfolioItems = document.querySelectorAll('.portfolio-item'),
+        popup = document.querySelector('.portfolio-popup'),
+        btnClose = document.querySelector('.popup-close'),
+        portfolioDetailsContent = document.querySelector('.popup-details'),
+        btnProjectDetails = document.querySelector('.popup-details-btn');
+
+    let itemIndex;
+    // filtrar portafolio
+    filterContainer.addEventListener('click', (event)=>{
+        if(event.target.classList.contains('filter-item') && !event.target.classList.contains('active')){
+            // Retirar la clase 'active' del item activo
+            filterContainer.querySelector('.active').classList.remove('active');
+            // Agregar la clase 'active' al 'filter-item' en uso
+            event.target.classList.add('active');
+            
+            const dataFilterItem = event.target.getAttribute('data-item');
+            console.log(dataFilterItem);
+            portfolioItems.forEach(element => {
+                // console.log('categoria', element.getAttribute('data-category'));
+                if(dataFilterItem === element.getAttribute('data-category') || dataFilterItem === 'all'){
+                    element.classList.remove('hide');
+                    element.classList.add('show');
+                }else{
+                    element.classList.remove('show');
+                    element.classList.add('hide');
+                }
+            });
+        }
+    });
+
+    // Abrir popup
+    portfolioItemsContainer.addEventListener('click', (event)=>{
+        // Devuelve el elemento ascendiente mas cercano que sea igual a 'portfolio-item-inner'
+        // console.log(event.target.closest('.portfolio-item-inner'));
+        if(event.target.closest('.portfolio-item-inner')){
+            const portfolioItem = event.target.closest('.portfolio-item-inner').parentElement;
+            // Obtener el indice de portfolioItem
+            itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
+            console.log(itemIndex);
+        }
+    });
+
+})();
